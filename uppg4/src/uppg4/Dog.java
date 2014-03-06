7bd2b9e1fd08b888195cfc6fae5d6d28cc1ae33f
Kalong <kalongdulkhadir@gmail.com>
2014-03-06 23:45:56 +0100

    Laboration 4

---
+++ uppg4/src/uppg4/Dog.java
@@ -0,0 +1,19 @@
+package uppg4;
+//info om dog-name
+
+public class Dog {
+	private String name;
+
+	public Dog() {
+
+	}
+
+	public String getName() {
+		return name;
+
+	}
+	public void setName(String _name){
+		name= _name;
+	}
+
+}
